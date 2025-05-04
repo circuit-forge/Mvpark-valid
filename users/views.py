2744178e534f980603329b4b83de7b25c5f30bf8
SelvaRagaavan <selvaragavan925@gmail.com>
2025-05-04 11:26:03 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -28,6 +28,9 @@ def join_waitlist(request):
 def show_waitlist(request):
     emails = [entry.email for entry in Waitlist.objects.all()]
     return HttpResponse("<br>".join(emails))
+def count_signups(request):
+    count = len(emails)
+    return HttpResponse(<h1> ' Total signups ' , count<h1>)
 
 
 
