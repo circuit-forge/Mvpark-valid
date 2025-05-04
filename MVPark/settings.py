f23d7efe32668aed995107bd0e8050aebacc464f
SelvaRagaavan <selvaragavan925@gmail.com>
2025-05-04 10:38:28 +0530

    Update settings.py

--- MVPark/settings.py
+++ MVPark/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-89!00sw$4$=ktct7!lf-23_8!k7y#u*pr1$f&(#e*w@svbi1^-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
